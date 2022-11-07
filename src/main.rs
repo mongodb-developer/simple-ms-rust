474600fab8cbd82e7a7385234b643d2f6617d91d
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-11-07 16:14:11 +0100

    Define custom behavior for unmatched routes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod in_mem_order_store;
 mod order_store;
 
-use axum::{routing::get, Router, Server};
+use axum::{
+    handler::Handler,
+    http::{StatusCode, Uri},
+    response::IntoResponse,
+    routing::get,
+    Router, Server,
+};
 
 #[tokio::main]
 async fn main() {
     let server_addr = ([127, 0, 0, 1], 8080).into();
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .fallback(fallback_handler.into_service());
 
     println!("Launching server: http://{server_addr}/");
     Server::bind(&server_addr)
@@ -25,4 +33,8 @@ async fn signal_shutdown() {
         .await
         .expect("expect tokio signal ctrl-c");
     println!("signal shutdown");
+}
+
+async fn fallback_handler(uri: Uri) -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, format!("No route for {}", uri))
 }
