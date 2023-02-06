176ea94a7be812c0cd1de838e6b02546916f5527
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-02-06 14:38:41 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod in_mem_order_store;
 mod order_store;
+
 use axum::{
     http::{StatusCode, Uri},
     response::IntoResponse,
@@ -7,12 +8,16 @@ use axum::{
     Router, Server,
 };
 use dotenv::dotenv;
+use log::{debug, error, info};
 use std::env;
 
 #[tokio::main]
 async fn main() {
     // Load environment configuration from .env
     dotenv().expect("Set your configuration in a .env file");
+    // Init Logging
+    env_logger::init();
+
     let server_addr = env::var("SERVER").expect("Define SERVER=host:port in your .env");
     let server_addr = server_addr
         .parse()
@@ -21,7 +26,7 @@ async fn main() {
         .route("/", get(hello))
         .fallback(fallback_handler);
 
-    println!("Launching server: http://{server_addr}/");
+    info!("Launching server: http://{server_addr}/");
     Server::bind(&server_addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(signal_shutdown())
@@ -30,6 +35,7 @@ async fn main() {
 }
 
 async fn hello() -> &'static str {
+    debug!("Static reply");
     "SuperMicroService"
 }
 
@@ -37,9 +43,9 @@ async fn signal_shutdown() {
     tokio::signal::ctrl_c()
         .await
         .expect("expect tokio signal ctrl-c");
-    println!("signal shutdown");
 }
 
 async fn fallback_handler(uri: Uri) -> impl IntoResponse {
+    error!("No route for {}", uri);
     (StatusCode::NOT_FOUND, format!("No route for {}", uri))
 }
