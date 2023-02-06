7c3a17da045923a053c9c6dd483578acd5895e97
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-02-06 16:32:42 +0100

    Use middleware for tracing and timeout

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ mod in_mem_order_store;
 mod order_store;
 
 use axum::{
+    error_handling::HandleErrorLayer,
     http::{StatusCode, Uri},
     response::IntoResponse,
     routing::get,
     Router, Server,
 };
 use dotenv::dotenv;
-use std::env;
+use std::{env, time::Duration};
+use tower::{timeout::TimeoutLayer, ServiceBuilder};
+use tower_http::trace::TraceLayer;
 use tracing::{debug, error, info};
 
 #[tokio::main]
@@ -24,6 +27,14 @@ async fn main() {
         .expect("Define SERVER=host:port in your .env");
     let app = Router::new()
         .route("/", get(hello))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(HandleErrorLayer::new(|_| async {
+                    StatusCode::REQUEST_TIMEOUT
+                }))
+                .layer(TimeoutLayer::new(Duration::from_secs(5))),
+        )
         .fallback(fallback_handler);
 
     info!("Launching server: http://{server_addr}/");
@@ -36,6 +47,7 @@ async fn main() {
 
 async fn hello() -> &'static str {
     debug!("Static reply");
+    tokio::time::sleep(Duration::from_secs(6)).await;
     "SuperMicroService"
 }
 
@@ -45,7 +57,7 @@ async fn signal_shutdown() {
         .expect("expect tokio signal ctrl-c");
 }
 
-#[tracing::instrument]
+//#[tracing::instrument]
 async fn fallback_handler(uri: Uri) -> impl IntoResponse {
     error!("No route for {}", uri);
     (StatusCode::NOT_FOUND, format!("No route for {}", uri))
