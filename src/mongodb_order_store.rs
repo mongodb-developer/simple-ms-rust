e4248d4ebb97e2c42cbad2b90d0ca363c4bb8045
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-02-07 13:31:53 +0100

    Create a MongoDB storage implementation

---
+++ src/mongodb_order_store.rs
@@ -0,0 +1,27 @@
+use mongodb::{
+    options::{ClientOptions, ResolverConfig},
+    Client,
+};
+
+use crate::order_store::OrderStoreError;
+
+pub struct MongodbOrderStore {
+    client: Client,
+}
+
+impl MongodbOrderStore {
+    pub async fn new(client_uri: &str) -> Result<MongodbOrderStore, OrderStoreError> {
+        if let Ok(options) =
+            ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+                .await
+        {
+            if let Ok(client) = Client::with_options(options) {
+                Ok(MongodbOrderStore { client })
+            } else {
+                Err(OrderStoreError::StoreUnavailable)
+            }
+        } else {
+            Err(OrderStoreError::StoreUnavailable)
+        }
+    }
+}
