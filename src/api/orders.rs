9b37a73b0b84239a4175a309592fcb3e9c160c4a
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-02-07 13:03:56 +0100

    Share abstraction (trait) instead of actual implementation

--- src/api/orders.rs
+++ src/api/orders.rs
@@ -7,14 +7,15 @@ use std::sync::Arc;
 use tracing::debug;
 use uuid::Uuid;
 
-use crate::{in_mem_order_store::InMemOrderStore, order_store::OrderStore};
+use crate::order_store::OrderStore;
 
 use super::{request::AddItem, response::Order};
 
-type DataState = Arc<InMemOrderStore>;
+type DataState = Arc<dyn OrderStore + Sync + Send>;
 
 const USER_ID: Uuid = Uuid::from_u128(0x5afb91d8_555d_45d7_a517_ece1b6655b42);
 
+//#[axum::debug_handler]
 pub async fn create(State(state): State<DataState>) -> (StatusCode, Json<Option<Order>>) {
     debug!("Creating order");
     if let Ok(order) = state.create_order(USER_ID).await {
